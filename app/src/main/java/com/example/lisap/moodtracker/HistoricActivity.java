e3e228eb710d60ab63e67c7032668593e0ab1703
lisa <39152394+lisa2308@users.noreply.github.com>
2018-08-01 10:37:36 +0200

    correction historiactivity

--- app/src/main/java/com/example/lisap/moodtracker/HistoricActivity.java
+++ app/src/main/java/com/example/lisap/moodtracker/HistoricActivity.java
@@ -1,62 +1,71 @@
 package com.example.lisap.moodtracker;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.util.Calendar;
 
-public class HistoricActivity {
+public class HistoricActivity extends AppCompatActivity {
 
-    Calendar cal = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.historic_activity);
 
-    for(int i=0; i<7;i++){
+//        Calendar cal = Calendar.getInstance();
+//
+//        for(int i=0; i<7;i++){
+//
+//            Cal.add(Calendar.DATE,-1)
+//
+//            int day = Calendar.getInstance().get(Calendar.DATE);
+//            int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+//            int year =  Calendar.getInstance().get(Calendar.YEAR);
+//
+//            String date = day + "" + month + "" + year;
+//            Log.e("tag",day+"");
+//
+//
+//            day = cal.get(Calendar.DATE);
+//            date = day+""+month+""+year;
+//
+//            Log.e("tag",date+"");
+//
+//            Cal.add(Calendar.DATE,-1)
+//
+//            day = cal.get(Calendar.DATE);
+//            date = day+""+month+""+year;
+//
+//            Log.e("tag",date+"");
+//
+//            Cal.add(Calendar.DATE,-1)
+//
+//            day = cal.get(Calendar.DATE);
+//            date = day+""+month+""+year;
+//
+//            Log.e("tag",date+"");
+//
+//            Cal.add(Calendar.DATE,-1)
+//
+//            day = cal.get(Calendar.DATE);
+//            date = day+""+month+""+year;
+//
+//            Log.e("tag",date+"");
+//
+//            Cal.add(Calendar.DATE,-1)
+//
+//            day = cal.get(Calendar.DATE);
+//            date = day+""+month+""+year;
+//
+//            Log.e("tag",date+"");
+//
+//            Cal.add(Calendar.DATE,-1)
+//
+//            day = cal.get(Calendar.DATE);
+//            date = day+""+month+""+year;
+//
+//            Log.e("tag",date+"");}
+    }
 
-        Cal.add(Calendar.DATE,-1)
-
-    int day = Calendar.getInstance().get(Calendar.DATE);
-    int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
-    int year =  Calendar.getInstance().get(Calendar.YEAR);
-
-    String date = day + "" + month + "" + year;
-    Log.e("tag",day+"");
-
-
-    day = cal.get(Calendar.DATE);
-    date = day+""+month+""+year;
-
-    Log.e("tag",date+"");
-
-     Cal.add(Calendar.DATE,-1)
-
-    day = cal.get(Calendar.DATE);
-    date = day+""+month+""+year;
-
-    Log.e("tag",date+"");
-
-     Cal.add(Calendar.DATE,-1)
-
-    day = cal.get(Calendar.DATE);
-    date = day+""+month+""+year;
-
-    Log.e("tag",date+"");
-
-     Cal.add(Calendar.DATE,-1)
-
-    day = cal.get(Calendar.DATE);
-    date = day+""+month+""+year;
-
-    Log.e("tag",date+"");
-
-     Cal.add(Calendar.DATE,-1)
-
-    day = cal.get(Calendar.DATE);
-    date = day+""+month+""+year;
-
-    Log.e("tag",date+"");
-
-     Cal.add(Calendar.DATE,-1)
-
-    day = cal.get(Calendar.DATE);
-    date = day+""+month+""+year;
-
-    Log.e("tag",date+"");}
 }
